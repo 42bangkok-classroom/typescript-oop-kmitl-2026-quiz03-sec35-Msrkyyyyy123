7927aabf0ecca35b5cbf4e2931db25ce892e5f1b
anuwuth <anuwuth.ch@gmail.com>
2026-02-16 10:02:06 +0700

    aaa

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,18 @@
-export class Person {}
+export class Person {
+    static COUNTRY : string = "Thailand"
+    firstname? : string ;
+    lastname? : string ;
+    private setAge? : number ;
+
+    setAge(age = number){
+        this.age = age;
+    }
+
+    getAge(): number {
+        return this.age ;
+    }
+
+    getFullName() {
+        return (this.firstname + " " + this.lastname);
+    }
+}
